e569c84c5152c62e3926eedba21b96fc4f24012d
Kuber Patel <kuberpatel@Kubers-MacBook-Air.local>
2025-03-16 01:46:14 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Hi there</h2>
+      <Button>Subscribe</Button>
+    </div>
+  );
+}
---
+++ app/provider.tsx
@@ -0,0 +1,30 @@
+"use client"
+import React from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+
+function Provider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+     const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+  return (
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+      <ConvexProvider client={convex}>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem={true}
+      disableTransitionOnChange={true}
+    >
+      {children}
+    </NextThemesProvider>
+    </ConvexProvider>
+    </GoogleOAuthProvider>
+  );
+}
+
+export default Provider;
